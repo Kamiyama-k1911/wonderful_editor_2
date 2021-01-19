7965cb10c5e6c9d2055ad1082d833a97d3dac26d
Kota Kamiyama <k.kamikami1211@gmail.com>
2021-01-19 17:44:24 +0900

    add: factory_botなしでUserのテストを作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "ユーザー名が指定されている時" do
+    it "ユーザー作成に成功する" do
+      user = User.new(name: "hoge",email: "hoge@example.com",password: "hogehoge")
+      expect(user).to be_valid
+    end
+  end
+
+  context "ユーザー名が指定されていない時" do
+    it "ユーザー作成に失敗する" do
+      user = User.new(name: nil,email: "hoge@example.com",password: "hogehoge")
+      expect(user).to be_invalid
+    end
+  end
+
+  context "メールアドレスが指定されている時" do
+    it "ユーザー作成に成功する" do
+      user = User.new(name: "hoge",email: "hoge@example.com",password: "hogehoge")
+      expect(user).to be_valid
+    end
+  end
+
+  context "メールアドレスが指定されていない時" do
+    it "ユーザー作成に失敗する" do
+      user = User.new(name: "hoge",email: nil,password: "hogehoge")
+      expect(user).to be_invalid
+    end
+  end
 end
