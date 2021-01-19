9cab1cbff53ec040da02ef7ced7817f3d27f7a3a
Kota Kamiyama <k.kamikami1211@gmail.com>
2021-01-19 18:25:48 +0900

    fix: factory_botにデータを定義し、リファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,5 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, presence: true
+  validates :password, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,30 +1,30 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
+  let(:user) { build(:user) }
   context "ユーザー名が指定されている時" do
     it "ユーザー作成に成功する" do
-      user = User.new(name: "hoge",email: "hoge@example.com",password: "hogehoge")
       expect(user).to be_valid
     end
   end
 
   context "ユーザー名が指定されていない時" do
     it "ユーザー作成に失敗する" do
-      user = User.new(name: nil,email: "hoge@example.com",password: "hogehoge")
+      user.name = nil
       expect(user).to be_invalid
-    end
-  end
-
-  context "メールアドレスが指定されている時" do
-    it "ユーザー作成に成功する" do
-      user = User.new(name: "hoge",email: "hoge@example.com",password: "hogehoge")
-      expect(user).to be_valid
     end
   end
 
   context "メールアドレスが指定されていない時" do
     it "ユーザー作成に失敗する" do
-      user = User.new(name: "hoge",email: nil,password: "hogehoge")
+      user.email = nil
+      expect(user).to be_invalid
+    end
+  end
+
+  context "パスワードが指定されていない時" do
+    it "ユーザー作成に失敗する" do
+      user.password = nil
       expect(user).to be_invalid
     end
   end
