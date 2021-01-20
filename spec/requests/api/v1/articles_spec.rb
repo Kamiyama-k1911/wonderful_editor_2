f41073b50d5254ec549be47837712a3f40e9666f
Kota Kamiyama <k.kamikami1211@gmail.com>
2021-01-20 16:15:35 +0900

    fix: rubocopの指摘箇所を修正

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -14,11 +14,11 @@ RSpec.describe "Articles", type: :request do
       res = JSON.parse(response.body)
 
       expect(res.length).to eq 3
-      expect(res[0].keys).to eq ["id","title","updated_at","user"]
+      expect(res[0].keys).to eq ["id", "title", "updated_at", "user"]
       expect(res.map {|d| d["id"] }).to eq [article3.id, article1.id, article2.id]
       expect(res[0]["user"].length).to eq 3
-      expect(res[0]["user"].keys).to eq ["id","name","email"]
-      expect(response).to have_http_status 200
+      expect(res[0]["user"].keys).to eq ["id", "name", "email"]
+      expect(response).to have_http_status :ok
     end
   end
 end
