bb7f49fc84cfc195521fd394849485761c9921b6
Kota Kamiyama <k.kamikami1211@gmail.com>
2021-01-21 22:53:25 +0900

    fix: rubocopの指摘箇所を修正

--- spec/requests/api/v1/auth/registrations_spec.rb
+++ spec/requests/api/v1/auth/registrations_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 RSpec.describe "Api::V1::Auth::Registrations", type: :request do
-  fdescribe "POST /api/v1/auth" do
-    subject { post(api_v1_user_registration_path,params: params) }
+  describe "POST /api/v1/auth" do
+    subject { post(api_v1_user_registration_path, params: params) }
 
     context "正しいパラメータが渡された時" do
       let(:params) { attributes_for(:user) }
