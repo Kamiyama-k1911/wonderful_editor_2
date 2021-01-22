575c1b0ac75495b416e29deef438765e25f06e4f
Kota Kamiyama <k.kamikami1211@gmail.com>
2021-01-22 21:20:04 +0900

    fix: 解答を基に修正

--- spec/requests/api/v1/auth/sessions_spec.rb
+++ spec/requests/api/v1/auth/sessions_spec.rb
@@ -4,38 +4,47 @@ RSpec.describe "Api::V1::Auth::Sessions", type: :request do
   describe "POST /api/v1/auth/sign_in" do
     subject { post(api_v1_user_session_path, params: params) }
 
-    let!(:user) { create(:user) }
-    context "正しいパラメータが渡された時" do
-      let(:params) { {password: user.password, email: user.email} }
+    let(:user) { create(:user) }
+    context "登録済のユーザー情報を送信した時" do
+      let(:params) { attributes_for(:user,password: user.password, email: user.email) }
 
       it "ログインできる" do
-        subject
-        res = JSON.parse(response.body)
-
-        expect(response.status).to eq 200
-        expect(res["data"]["name"]).to eq user.name
-        expect(res["data"]["email"]).to eq user.email
-      end
-
-      it "header情報を取得することができる" do
         subject
         header = response.header
         expect(header["access-token"]).to be_present
         expect(header["client"]).to be_present
-        expect(header["expiry"]).to be_present
         expect(header["uid"]).to be_present
-        expect(header["token-type"]).to be_present
+        expect(response).to have_http_status(:ok)
       end
     end
 
-    fcontext "正しくないパラメータが渡された時" do
-      let(:params) { attributes_for(:user) }
+    context "emailが一致しない時" do
+      let(:params) { attributes_for(:user,email: "hogehoge",password: user.password) }
 
       it "ログインできない" do
         subject
-        binding.pry
         res = JSON.parse(response.body)
+        header = response.header
 
+        expect(header["access-token"]).to be_blank
+        expect(header["client"]).to be_blank
+        expect(header["uid"]).to be_blank
+        expect(response).to have_http_status :unauthorized
+        expect(res["errors"]).to include "Invalid login credentials. Please try again."
+      end
+    end
+
+    context "passwordが一致しない時" do
+      let(:params) { attributes_for(:user,email: user.email ,password: "aaaaaaaaaaaaaa") }
+
+      it "ログインできない" do
+        subject
+        res = JSON.parse(response.body)
+        header = response.header
+
+        expect(header["access-token"]).to be_blank
+        expect(header["client"]).to be_blank
+        expect(header["uid"]).to be_blank
         expect(response).to have_http_status :unauthorized
         expect(res["errors"]).to include "Invalid login credentials. Please try again."
       end
