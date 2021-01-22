a9b80757b6c51b7446464b8993ef2958da5d8c0e
Kota Kamiyama <k.kamikami1211@gmail.com>
2021-01-23 02:40:04 +0900

    add: 各種メソッドを実装

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::BaseApiController < ApplicationController
+  def current_user
+    current_api_v1_user
+  end
+
+  def authenticate_user!
+    authenticate_api_v1_user!
+  end
+
+  def user_signed_in?
+    api_v1_user_signed_in?
+  end
 end
--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "Articles", type: :request do
+RSpec.fdescribe "Articles", type: :request do
   describe "GET /api/v1/articles" do
     subject { get(api_v1_articles_path) }
 
