2ac9d9b9331acc62933a4e00ffed1e89d1e35e99
Kota Kamiyama <k.kamikami1211@gmail.com>
2021-01-23 03:20:48 +0900

    fix: 解答を基に修正

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -1,13 +1,5 @@
 class Api::V1::BaseApiController < ApplicationController
-  def current_user
-    current_api_v1_user
-  end
-
-  def authenticate_user!
-    authenticate_api_v1_user!
-  end
-
-  def user_signed_in?
-    api_v1_user_signed_in?
-  end
+  alias_method :current_user, :current_api_v1_user
+  alias_method :authenticate_user!, :authenticate_api_v1_user!
+  alias_method :user_signed_in?, :api_v1_user_signed_in?
 end
