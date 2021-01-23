26de29f362d269a1f9fd23b97f9721d43a8a435c
Kota Kamiyama <k.kamikami1211@gmail.com>
2021-01-24 04:00:34 +0900

    add: 下書き・公開記事取得のテストを実装

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,6 +2,8 @@ require "rails_helper"
 
 RSpec.describe Article, type: :model do
   let(:article) { build(:article) }
+  let!(:draft) { create_list(:article,4,article_status: "draft") }
+  let!(:published) { create_list(:article,3,article_status: "published") }
 
   context "タイトルが指定されている時" do
     it "記事が投稿できる" do
@@ -24,6 +26,22 @@ RSpec.describe Article, type: :model do
 
       expect(article).to be_invalid
       expect(article.errors.details[:body][0][:error]).to eq :blank
+    end
+  end
+
+  context "下書き記事だけ取得しようとした時" do
+    it "取得できる" do
+      search_result_count = Article.where(article_status: "draft").count
+
+      expect(search_result).to eq 4
+    end
+  end
+
+  context "公開記事だけ取得しようとした時" do
+    it "取得できる" do
+      search_result_count = Article.where(article_status: "published").count
+
+      expect(search_result).to eq 3
     end
   end
 end
