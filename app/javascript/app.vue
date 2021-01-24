308875320ce1b00b85e099d11a9058a8c3a65bb8
Kota Kamiyama <k.kamikami1211@gmail.com>
2021-01-24 10:34:27 +0900

    add: フロントエンドとの繋ぎ込みを実装

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,87 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
-  </div>
+  <v-app id="app">
+    <dir class="header">
+      <Header/>
+    </dir>
+    <div class="layout">
+      <router-view></router-view>
+    </div>
+  </v-app>
 </template>
 
 <script>
+import Header from "./components/layout/Header.vue"
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
     }
+  },
+
+  components: {
+    Header,
   }
 }
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
+.header {
+  margin-bottom: 70px;
+}
+
+.layout {
+  background: #ECF6FE
+}
+</style>
+
+<style lang="scss">
+.v-application code, .v-application kbd {
+  border-radius: initial !important;
+  font-size: initial !important;
+  font-weight: initial !important;
+}
+
+.hljs {
+  background-color: #23241f !important;
+  padding: 0.5em !important;
+}
+
+.hljs, .hljs-tag, .hljs-subst {
+    color: #fff !important;
+}
+
+/* highlightjs の 1行目のずれを強制 */
+
+code:before {
+  content: none !important;
+}
+
+.hljs span:first-child {
+  position: relative;
+  left: -2px;
+}
+
+
+
+.preview, .article_detail {
+  h2 {
+    border-bottom: 1px solid rgba(0,0,0,.42);
+    margin: 30px 0 10px;
+  }
+
+  h3 {
+    margin: 30px 0 10px;
+  }
+
+  h4 {
+    margin: 15px 0 10px;
+  }
+
+  p {
+    margin: 20px 0;
+  }
+
+  pre {
+  margin: 10px 0;
+  }
 }
 </style>
